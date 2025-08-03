18de9c51aa968953114ca3bc0f2117d77d22a787
Utku Çakar <cakarutku1@hotmail.com>
2025-08-03 19:41:13 +0300

    added nhibernate productdal

---
+++ DevFrameWork.Core/DevFramework.Northwind.DataAccess/Concerete/NHibernate/NhProductDal.cs
@@ -0,0 +1,13 @@
+﻿using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concerete;
+using DevFrameWork.Core.DataAccess.NHibernate;
+
+namespace DevFramework.Northwind.DataAccess.Concerete.NHibernate
+{
+    public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
+    {
+        public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
