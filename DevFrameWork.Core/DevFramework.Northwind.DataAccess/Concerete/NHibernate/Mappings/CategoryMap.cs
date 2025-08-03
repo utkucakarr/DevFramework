6b7d50a83f2d5ef71830d8487d1098f24ad8227e
Utku Çakar <cakarutku1@hotmail.com>
2025-08-03 20:18:50 +0300

    integrated category for nhibernaste

---
+++ DevFrameWork.Core/DevFramework.Northwind.DataAccess/Concerete/NHibernate/Mappings/CategoryMap.cs
@@ -0,0 +1,18 @@
+﻿using DevFramework.Northwind.Entities.Concerete;
+using FluentNHibernate.Mapping;
+
+namespace DevFramework.Northwind.DataAccess.Concerete.NHibernate.Mappings
+{
+    public class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            // Mapping
+            Table("Products");
+            LazyLoad();
+            Id(x => x.CategoryID).Column("CategoryID");
+            Map(x => x.CategoryID).Column("CategoryID");
+            Map(x => x.CategoryName).Column("CategoryName");
+        }
+    }
+}
---
+++ DevFrameWork.Core/DevFramework.Northwind.DataAccess/Concerete/NHibernate/NhCategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concerete;
+using DevFrameWork.Core.DataAccess.NHibernate;
+
+namespace DevFramework.Northwind.DataAccess.Concerete.NHibernate
+{
+    public class NhCategoryDal : NhEntityRepositoryBase<Category>, ICategoryDal
+    {
+        public NhCategoryDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+        }
+    }
+}
