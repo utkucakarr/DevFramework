24065740021e4e740b8ec855ddcef1d114545ee6
Utku Çakar <cakarutku1@hotmail.com>
2025-08-03 19:17:51 +0300

    added product map for Nhibernate

---
+++ DevFrameWork.Core/DevFramework.Northwind.DataAccess/Concerete/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using DevFramework.Northwind.Entities.Concerete;
+using FluentNHibernate.Mapping;
+
+namespace DevFramework.Northwind.DataAccess.Concerete.NHibernate.Mappings
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            // Mapping
+            Table("Products");
+            LazyLoad();
+            Id(x => x.ProductID).Column("ProductID");
+            Map(x => x.CategoryID).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+        }
+    }
+}
