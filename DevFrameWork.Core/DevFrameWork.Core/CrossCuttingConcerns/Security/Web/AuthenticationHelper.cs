633bbffc958595f20961b8b5db84ca72a4215345
Utku Çakar <cakarutku1@hotmail.com>
2025-08-07 17:33:39 +0300

    applied authenticationHelper codes

--- DevFrameWork.Core/DevFrameWork.Core/CrossCuttingConcerns/Security/Web/AuthenticationHelper.cs
+++ DevFrameWork.Core/DevFrameWork.Core/CrossCuttingConcerns/Security/Web/AuthenticationHelper.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Web.Security;
 
 namespace DevFrameWork.Core.CrossCuttingConcerns.Security.Web
 {
-    internal class AuthenticationHelper
+    public class AuthenticationHelper
     {
+        public static void CreateAuthCookie(int id, string userName, string email, DateTime expiration, string[] roles,
+            bool rememberMe, string firstName, string lastName)
+        {
+            var authTicket = new FormsAuthenticationTicket(1,userName,DateTime.Now,expiration,rememberMe,
+                CreateAuthTags(email,roles,firstName,lastName,id));
+            // Encrypt ediyoruz
+            string encTicket = FormsAuthentication.Encrypt(authTicket);
+            HttpContext.Current.Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encTicket));
+        }
+
+        private static string CreateAuthTags(string email, string[] roles, string firstName, string lastName, int id)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(email);
+            stringBuilder.Append("|");
+            for (int i = 0; i < roles.Length; i++)
+            {
+                stringBuilder.Append(roles[i]);
+                if (i < roles.Length)
+                {
+                    stringBuilder.Append(",");
+                }
+            }
+            stringBuilder.Append("|");
+            stringBuilder.Append(firstName);
+            stringBuilder.Append("|");
+            stringBuilder.Append(lastName);
+            stringBuilder.Append("|");
+            stringBuilder.Append(id);
+            return stringBuilder.ToString();
+        }
     }
 }
