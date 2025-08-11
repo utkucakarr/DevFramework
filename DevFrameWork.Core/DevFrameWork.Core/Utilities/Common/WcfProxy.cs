a6433ce3dbcd9c5200d65f5a064886e11dc9cb09
Utku Çakar <cakarutku1@hotmail.com>
2025-08-11 14:34:15 +0300

    implemented channel factor

---
+++ DevFrameWork.Core/DevFrameWork.Core/Utilities/Common/WcfProxy.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate.Linq.ReWriters;
+using System.Configuration;
+using System.ServiceModel;
+
+namespace DevFrameWork.Core.Utilities.Common
+{
+    public class WcfProxy<T>
+    {
+        public static T CreateChannel()
+        {
+            string baseAddress = ConfigurationManager.AppSettings["ServiceAddress"];
+            string address = string.Format(baseAddress, typeof(T).Name.Substring(1));
+
+            var binding = new BasicHttpBinding();
+            var channel = new ChannelFactory<T>(binding, address);
+
+            return channel.CreateChannel();
+        }
+    }
+}
