93807ad62fc9fdc55d0ff7550619458217f6c0a1
Utku Çakar <cakarutku1@hotmail.com>
2025-08-11 14:47:36 +0300

    packages configuration

---
+++ DevFrameWork.Core/DevFramework.Northwind.Business/DependencyResolvers/Ninject/ServiceModule.cs
@@ -0,0 +1,14 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFrameWork.Core.Utilities.Common;
+using Ninject.Modules;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class ServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().ToConstant(WcfProxy<IProductService>.CreateChannel());
+        }
+    }
+}
